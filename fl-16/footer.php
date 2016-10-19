69ad9d9c01c10746b698346b0a86659d16e53ead
milo <me@milo.sh>
2016-10-18 19:40:16 -0700

    SHC theme
    all theme files for SHC

---
+++ fl-16/footer.php
@@ -0,0 +1,10 @@
+    <div id="#">
+        <ul>
+            <li><a href="javascript:;"></a></li>
+        </ul>
+    </div>
+        <!-- WP footer begin -->
+        <?php wp_footer(); ?>
+        <!-- WP footer eng -->
+    </body>
+</html>
---
+++ fl-16/page.php
@@ -0,0 +1,14 @@
+  <?php get_header(); ?>
+    <!-- WP content start -->
+<div id="#">
+    <div id="#">
+    <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+    <h2><?php the_title(); ?></h2>     
+    <?php the_content(''); ?>
+    <?php endwhile; endif; ?>
+        <p>page.php</p>
+    </div>
+<?php get_sidebar();?>
+</div>
+<!-- WP content end -->
+<?php get_footer(); ?>
