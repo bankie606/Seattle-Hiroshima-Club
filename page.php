628a888e84a7aba92e49fbe549bc7c73c2d7e9da
milo <me@milo.sh>
2016-10-28 16:25:18 -0700

    sidebar
    added sidebar, along with styles for it and the "main content" div so
    they can fit next to eachother.

--- page.php
+++ page.php
@@ -1,12 +1,17 @@
-  <?php get_header(); ?>
-    <!-- WP content start -->
+<?php get_header(); ?>
+<!-- WP content start -->
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
 <h1><?php the_title(); ?></h1>  
-        <div class="entry">
+        <div class="page-content">
             <?php the_content(); ?>
+            <p>page.php</p>
         </div><!-- entry -->
+        <div class="page-sidebar">
+            <?php get_sidebar(); ?>
+        </div>
+        
 <?php endwhile; ?>
 <?php endif; ?>
+
 <!-- WP content end -->
 <?php get_footer(); ?>
--- sidebar.php
+++ sidebar.php
@@ -1,52 +1,38 @@
-<div id="sidebar">
-    <div id="#">
-
         <?php 
 
         if(is_page()) {
 
             if($post->post_parent){// check if page is parent...
 
-                echo '<h2>'.get_the_title($post->post_parent).'</h2>';
+                echo '<h3>'.get_the_title($post->post_parent).'</h3>';
                 echo '<ul>';
                 wp_list_pages(array('title_li' => '', 'child_of' => $post->post_parent,)); //... list current page
                 echo '</ul>';
             } else { // if page does not have a parent...
 
-                echo '<h2>'.get_the_title($post->ID).'</h2>';
+                echo '<h3>'.get_the_title($post->ID).'</h3>';
                 echo '<ul>';
                 wp_list_pages(array('title_li' => '', 'child_of' => $post->ID,));// ... do this
                 echo '</ul>';
             }
-
+            
+            echo '<h3>Events</h3>';
+            
+            echo '<a href="/donate"><button type="button" class="btn btn-block">donate</button></a>';
+            echo '<h3>Newsletter</h3>';
+            echo '<form class="navbar-form navbar-left" role="search">
+                    <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Sign up for our mailing list">
+                    </div>
+                    </form>';
+            
         }
 
         if(!(is_page())) {
 
-            echo '<h2>Blog</h2>';
+            echo '<h3>Blog</h3>';
             echo '<ul>';
             wp_list_categories(array('title_li' => '',));
             echo '</ul>';
             }
         ?>
-    </div>
-    
-    <!-- quote start -->
-    <div id="my-quote">
-    <?php if (get_post_meta($post->ID, 'quote', true)) : ?>
-            
-   <blockquote><?php echo get_post_meta($post->ID, 'quote', true);?></blockquote>
-        
-    <?php endif; ?>
-    </div>
-    <!-- quote end -->
-    
-    
-    <!-- dynamic widget start -->
-    <div id="dynamic-widgets">
-    <?php dynamic_sidebar(); ?>
-    </div>
-    
-    <!-- dynamic widget end -->
-    
-</div>
