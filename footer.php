841e1677c0047747fb8d0f9ff234e86fbadfc34c
bankie606 <frcrescioni@gmail.com>
2016-11-08 20:47:19 -0800

    Search Bar Added to Footer
    Added a search bar to the footer to search for stuff within the site.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div id="shc-footer panel">
     <div class="row panel-footer">
         <div class="col-md-4 shc-footer-main-info">
-            <img src="<?php bloginfo('template_directory');?>/images/shc_logo.png" class="img-circle" />       
+            <img src="http://www.seattle-hc.frcrescioni.net/wp-content/uploads/2016/11/shc_logo.png" class="img-circle" />       
                 <h4 class="#">Seattle Hiroshima Club</h4>
                 <h6 class="#">1000 Main St, Seattle WA 98104</h6>
                 <h6 class="#">1 800 636 252</h6>
@@ -17,20 +17,24 @@
             <?php wp_nav_menu( array ( 'theme_location' => 'footer-pt2' )); ?>
         </div>
         <div class="col-md-4 shc-footer-last">
-            <ul class="shc-nav-footer">
-            <li class="footer-contact-nav"><a href="/contact">Contact</a></li>
-            <li>Linda Ishii</li>
-            <li>seattlehiroshimaclub@gmail.com</li>
-            <li>360 787 9020</li>
-            </ul>
+            <form method="get" id="searchform" action="http://www.seattle-hc.frcrescioni.net">
+						<p class="srch submit">
+							<input type="text" class="srch-txt" value="" name="s" id="s" size="30">
+							<input type="submit" class="button" id="searchsubmit" value="Run Test Search">
+						</p>
+					</form>
             <span>
                 <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
                 <i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i>                
                 <i class="fa fa-instagram fa-3x" aria-hidden="true"></i>
                 <i class="fa fa-youtube-play fa-3x" aria-hidden="true"></i>
             </span>
-            <h3>Search Bar Here</h3>
-            
+            <ul class="shc-nav-footer">
+            <li class="footer-contact-nav"><a href="/contact">Contact</a></li>
+            <li>Linda Ishii</li>
+            <li>seattlehiroshimaclub@gmail.com</li>
+            <li>360 787 9020</li>
+            </ul>
             
             
         </div>
