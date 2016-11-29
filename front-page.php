900e4420e0d2af3161e8b2c8aae43827e75846c1
ameyers89 <ameyer12@seattlecentral.edu>
2016-11-28 22:03:39 -0800

    front-page cta's and text banner
    added cta and text banner styles for front-page template

--- front-page.php
+++ front-page.php
@@ -4,32 +4,41 @@
     <?php if ( have_posts() ) : while( have_posts() ) : the_post(); // start loop one ?>
         <?php the_content(''); // get the home page's content ?>
         <?php endwhile; endif; // end loop one ?>
-        <div id="cta">
-        <div class="box">
-            <div class="cta-text">
-                <h3>Who we are</h3>
-                <img src="<?php bloginfo('template_directory');?>/images/cta1v2.gif"  />
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitDuis non mauris libero. Suspendisse faucibus aliquam lacus nec porttitor.</p>
-                    <a href="#">Learn More ></a>
+    
+        <div class="front-page-banner">
+            <div class="banner-container">
+                <h2>Heritage. Education. Community.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet nibh nisl. Mauris ac urna a neque accumsan tempus vel pellentesque sem.</p>
             </div>
         </div>
-        <div class="box">
-            <div class="cta-text">
-                <h3>What we do</h3>
-                <img src="<?php bloginfo('template_directory');?>/images/cta2v2.gif"  />
-                <p>pretium euismod sem. Sed accumsan laoreet pretium. Pellentesque bibendum mi id libero faucibus, nec mattis arcu bibendum Fusce.</p>
-                <a href="#">Learn More ></a>
-            </div>
-        </div>
-        <div class="box">
-            <div class="cta-text">
-                <h3>Get Involved</h3>
-                <img src="<?php bloginfo('template_directory');?>/images/cta3v2.gif"  />
-                <p>Aliquam non nisl et nunc volutpat tempor eget eu tortor. Donec eu mauris lorem. Curabitur porttitor volutpat orci quis dapibus.</p>
-                <a href="#">Learn More ></a>
-            </div>
-        </div>
-    </div>
+    
+        <div class="grid">
+            <figure class="effect-chico">
+                <img src="<?php bloginfo('template_directory');?>/images/cta-forest.jpeg"/>
+                <figcaption>
+                    <h2>Who We Are</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitDuis non mauris libero.</p>
+                    <a href="#">View more</a>
+                </figcaption>			
+            </figure>
+            <figure class="effect-chico">
+                <img src="<?php bloginfo('template_directory');?>/images/cta-lamp.jpg"/>
+                <figcaption>
+                    <h2>What We Do</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitDuis non mauris libero.</p>
+                    <a href="#">View more</a>
+                </figcaption>			
+            </figure>
+            <figure class="effect-chico">
+                <img src="<?php bloginfo('template_directory');?>/images/cta-building.jpg"/>
+                <figcaption>
+                    <h2>Get Involved</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitDuis non mauris libero.</p>
+                    <a href="#">View more</a>
+                </figcaption>			
+            </figure>
+        </div><!--end grid class-->
+    
 </div>
 <!-- WP content end -->
 <?php get_footer(); ?>
